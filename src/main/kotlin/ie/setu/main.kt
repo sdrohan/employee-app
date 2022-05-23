a0ee97f48de553e9401b20b3e77e7db15db86e44
Siobhan Drohan <sdrohan@wit.ie>
2022-05-23 19:55:16 +0100

    Utility method for rounding doubles to two decimal places

--- src/main/kotlin/ie/setu/main.kt
+++ src/main/kotlin/ie/setu/main.kt
@@ -1,5 +1,7 @@
 package ie.setu
 
+import kotlin.math.round
+
 val firstName = "Joe"
 val surname = "Soap"
 val gender = 'm'
@@ -13,17 +15,15 @@ val cycleToWorkMonthlyDeduction = 54.33
 fun main(args: Array<String>){
     println("Payslip Printer")
     printPayslip()
-    println("\n")
-    printPayslipWithRounding()
 }
 
 fun printPayslip(){
 
-    val monthlySalary = (grossSalary/12)
-    val monthlyPrsi = monthlySalary * (prsiPercentage / 100)
-    val monthlyPaye = monthlySalary * (payePercentage / 100)
-    val grossPay = (monthlySalary + (annualBonus/12))
-    val totalDeductions = (monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction)
+    val monthlySalary   = roundTwoDecimals(grossSalary/12)
+    val monthlyPrsi     = roundTwoDecimals(monthlySalary * (prsiPercentage / 100))
+    val monthlyPaye     = roundTwoDecimals(monthlySalary * (payePercentage / 100))
+    val grossPay        = roundTwoDecimals(monthlySalary + (annualBonus/12))
+    val totalDeductions = roundTwoDecimals((monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction))
 
     println ("_________________________________________________________________________")
     println ("|                          Monthly Payslip                              |")
@@ -35,44 +35,20 @@ fun printPayslip(){
     println ("|                                                                       |")
     println ("|    PAYMENT DETAILS \t\t\t\t DEDUCTION DETAILS  \t\t\t\t|")
     println ("|_______________________________________________________________________|")
-    println ("|    Salary: $monthlySalary \t\t PAYE: $monthlyPaye \t\t\t|")
-    println ("|    Bonus:  ${(annualBonus/12)} \t\t\t\t PRSI: $monthlyPrsi  \t\t\t|")
+    println ("|    Salary: $monthlySalary \t\t\t\t PAYE: $monthlyPaye \t\t\t\t\t\t|")
+    println ("|    Bonus:  ${roundTwoDecimals(annualBonus/12)} \t\t\t\t PRSI: $monthlyPrsi  \t\t\t\t\t\t|")
     println ("|    \t\t\t\t\t\t\t\t Cycle To Work: $cycleToWorkMonthlyDeduction \t\t\t\t|")
     println ("|_______________________________________________________________________|")
-    println ("|    Gross: $grossPay \t\tTotal Deductions: $totalDeductions |")
+    println ("|    Gross: $grossPay \t\t\t\tTotal Deductions: $totalDeductions \t\t\t|")
     println ("|_______________________________________________________________________|")
-    println ("|   \t\t\t\t NET PAY: ${(grossPay - totalDeductions)}  \t\t\t\t\t\t|")
+    println ("|   \t\t\t\t\t\t NET PAY: ${roundTwoDecimals(grossPay - totalDeductions)}  \t\t\t\t\t\t\t|")
     println ("|_______________________________________________________________________|")
 }
 
-fun printPayslipWithRounding(){
-
-    val monthlySalary = (grossSalary/12)
-    val monthlyPrsi = monthlySalary * (prsiPercentage / 100)
-    val monthlyPaye = monthlySalary * (payePercentage / 100)
-    val grossPay = (monthlySalary + (annualBonus/12))
-    val totalDeductions = (monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction)
-
-    println ("_________________________________________________________________________")
-    println ("|                          Monthly Payslip                              |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|   Employee Name:  ${firstName.uppercase()} ${surname.uppercase()} (${gender.uppercase()})                   Employee ID: $employeeID    |")
-    println ("|                                                                       |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|    PAYMENT DETAILS \t\t\t\t DEDUCTION DETAILS  \t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Salary: ${"%.2f".format(monthlySalary)} \t\t\t\t PAYE: ${"%.2f".format(monthlyPaye)} \t\t\t\t\t\t|")
-    println ("|    Bonus:  " + "%.2f".format((annualBonus / 12)) + "\t\t\t\t\t PRSI: " + "%.2f".format(monthlyPrsi)  + "\t\t\t\t\t\t|")
-    println ("|    \t\t\t\t\t\t\t\t Cycle To Work: $cycleToWorkMonthlyDeduction \t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Gross:  " + "%.2f".format(grossPay) + " \t\t\t\tTotal Deductions: " + "%.2f".format(totalDeductions) + " \t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|   \t\t\t\t\t\t NET PAY:" + "%.2f".format((grossPay - totalDeductions)) + " \t\t\t\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-}
-
+//https://discuss.kotlinlang.org/t/how-do-you-round-a-number-to-n-decimal-places/8843
+//There are several options...try each of them out
+fun roundTwoDecimals(number: Double) = round(number * 100) / 100
+//fun roundTwoDecimals(number: Double) = "%.2f".format(number).toDouble()
 
 
 
