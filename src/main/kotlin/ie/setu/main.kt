9baae3b484adf57399c0cfc3e9403089ccc033f4
Siobhan Drohan <sdrohan@wit.ie>
2022-05-31 16:00:30 +0100

    Enable user to Add a new employee

--- src/main/kotlin/ie/setu/main.kt
+++ src/main/kotlin/ie/setu/main.kt
@@ -2,11 +2,13 @@ package ie.setu
 
 import kotlin.math.round
 
-val employee =  Employee("Joe", "Soap", 'm', 6143,
+var employee =  Employee("Joe", "Soap", 'm', 6143,
     67543.21, 38.5, 5.2,
     1450.50, 54.33)
 
 fun main(args: Array<String>){
+
+    addEmployee()
 
     var input : Int
 
@@ -40,6 +42,29 @@ fun menu() : Int {
           -1. Exit
          Enter Option : """)
          return readLine()!!.toInt()
+}
+
+fun addEmployee(){
+    print("Enter first name: ")
+    val firstName = readLine().toString()
+    print("Enter surname: ")
+    val surname = readLine().toString()
+    print("Enter gender (m/f): ")
+    val gender = readLine()!!.toCharArray()[0]
+    print("Enter employee ID: ")
+    val employeeID = readLine()!!.toInt()
+    print("Enter gross salary: ")
+    val grossSalary = readLine()!!.toDouble()
+    print("Enter PAYE %: ")
+    val payePercentage = readLine()!!.toDouble()
+    print("Enter PRSI %: ")
+    val prsiPercentage = readLine()!!.toDouble()
+    print("Enter Annual Bonus: ")
+    val annualBonus= readLine()!!.toDouble()
+    print("Enter Cycle to Work Deduction: ")
+    val cycleToWorkMonthlyDeduction= readLine()!!.toDouble()
+
+    employee = Employee(firstName, surname, gender, employeeID, grossSalary, payePercentage, prsiPercentage, annualBonus, cycleToWorkMonthlyDeduction)
 }
 
 fun getFullName() = when (employee.gender){
