51c7b9f1ee036b79b2a361a1a0bb19a4d93323ba
Siobhan Drohan <sdrohan@wit.ie>
2026-05-29 17:09:11 +0100

    Add menu to app for displaying various data points to user

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,9 +12,45 @@ val bonusPercentage: Double = 4.5
 val taxRatePercentage: Double = 23.5
 val pensionContributionPercentage: Double = 6.7
 
-fun main() {
-    println("Pay Slip Printer")
-    println(getPayslip())
+fun main(){
+
+    var input : Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("Hourly Rate: $hourlyRate")
+            2 -> println("Hours Worked: $hoursWorked")
+            3 -> println("Overtime Hours: $overtimeHoursWorked, Amount Earned: ${calculateOvertimePay()}")
+            4 -> println("Bonus Amount: ${money(calculateBonus())}")
+            5 -> println("Tax Paid: ${money(calculateTax())} at rate $taxRatePercentage")
+            6 -> println("Pension Paid: ${money(calculatePension())} at rate $pensionContributionPercentage")
+            7 -> println("Gross Pay: ${money(calculateGrossPay())}")
+            8 -> println("Net pay: ${money(calculateNetPay())}")
+            9 -> println(getPayslip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+}
+
+
+fun menu() : Int {
+    print("""
+         Employee Menu for ${getFullName()}
+           1. Hourly Rate
+           2. Hours Worked
+           3. Overtime Hours
+           4. Bonus
+           5. Tax Rate
+           6. Pension
+           7. Gross Pay
+           8. Net Pay
+           9. Full Payslip
+          -1. Exit
+         Enter Option : """)
+    return readln().toInt()
 }
 
 fun getPayslip(): String {
