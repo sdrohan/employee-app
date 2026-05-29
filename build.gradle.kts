6458cb639c810ebd70509b0bed3f7bbc5d89d8be
Siobhan Drohan <sdrohan@wit.ie>
2026-05-29 15:08:12 +0100

    Employee App, Version 1

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.21"
+}
+
+group = "ie.setu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
