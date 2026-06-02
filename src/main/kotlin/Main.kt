eebb96c37116a9885e5164436aeee0a9f219f35c
Siobhan Drohan <sdrohan@wit.ie>
2026-06-02 12:16:30 +0100

    Create an add function to read Employee input from the console

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,6 +13,8 @@ var employee =  Employee(
 
 fun main(){
 
+    add()
+
     var input : Int
 
     do {
@@ -50,6 +52,56 @@ fun menu() : Int {
           -1. Exit
          Enter Option : """)
     return readln().toInt()
+}
+
+fun add() {
+
+    print("Enter employee ID: ")
+    val employeeId = readln().toInt()
+
+    print("Enter first name: ")
+    val firstName = readlnOrNull().toString()
+
+    print("Enter surname: ")
+    val surname = readlnOrNull().toString()
+
+    print("Enter department: ")
+    val department = readlnOrNull().toString()
+
+    print("Enter job title: ")
+    val jobTitle = readlnOrNull().toString()
+
+    print("Enter hourly rate: ")
+    val hourlyRate = readln().toDouble()
+
+    print("Enter hours worked: ")
+    val hoursWorked = readln().toInt()
+
+    print("Enter overtime hours worked: ")
+    val overtimeHoursWorked = readln().toInt()
+
+    print("Enter bonus percentage: ")
+    val bonusPercentage = readln().toDouble()
+
+    print("Enter tax rate percentage: ")
+    val taxRatePercentage = readln().toDouble()
+
+    print("Enter pension contribution percentage: ")
+    val pensionContributionPercentage = readln().toDouble()
+
+    employee = Employee(
+        employeeId,
+        firstName,
+        surname,
+        department,
+        jobTitle,
+        hourlyRate,
+        hoursWorked,
+        overtimeHoursWorked,
+        bonusPercentage,
+        taxRatePercentage,
+        pensionContributionPercentage
+    )
 }
 
 fun getPayslip(): String {
